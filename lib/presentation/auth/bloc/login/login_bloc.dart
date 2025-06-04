90d85a78f6a829f3f409f2325964ebe3fb50a3a5
Rangga857 <azharrangga28@gmail.com>
2025-06-05 04:21:40 +0700

    create bussines logic login

---
+++ lib/presentation/auth/bloc/login/login_bloc.dart
@@ -0,0 +1,27 @@
+import 'package:canaryfarm/data/model/response/auth_response_model.dart';
+import 'package:canaryfarm/data/repository/auth_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'login_event.dart';
+import 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final AuthRepository authRepository;
+
+  LoginBloc({required this.authRepository}) : super(LoginInitial()) {
+    on<LoginRequested>(_onLoginRequested);
+  }
+
+  Future<void> _onLoginRequested(
+    LoginRequested event,
+    Emitter<LoginState> emit,
+  ) async {
+    emit(LoginLoading());
+
+    final result = await authRepository.login(event.requestModel);
+
+    result.fold(
+      (l) => emit(LoginFailure(error: l)),
+      (r) => emit(LoginSuccess(responseModel: r as AuthResponseModel)),
+    );
+  }
+}
