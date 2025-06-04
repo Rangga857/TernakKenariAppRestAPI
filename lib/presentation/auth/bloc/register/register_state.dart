4e407e255527850340d597f140f9809f3eb2bed3
Rangga857 <azharrangga28@gmail.com>
2025-06-05 04:25:02 +0700

    create state sukses dan gagal register

--- lib/presentation/auth/bloc/register/register_state.dart
+++ lib/presentation/auth/bloc/register/register_state.dart
@@ -4,4 +4,12 @@ final class RegisterInitial extends RegisterState {}
 
 final class RegisterLoading extends RegisterState {}
 
+final class RegisterSuccess extends RegisterState {
+  final String message;
+  RegisterSuccess({required this.message});
+}
 
+final class RegisterFailure extends RegisterState {
+  final String error;
+  RegisterFailure({required this.error});
+}
