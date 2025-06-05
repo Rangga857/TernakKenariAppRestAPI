f5682957e1aad6f29172018fe27bef3a9baa185a
Rangga857 <azharrangga28@gmail.com>
2025-06-05 18:18:06 +0700

    create bussines logic getburung

---
+++ lib/presentation/bloc/get_all_burung_tersedia/get_all_burung_tersedia_bloc.dart
@@ -0,0 +1,27 @@
+import 'package:canaryfarm/data/repository/get_all_burung_tersedia_repository.dart';
+import 'package:canaryfarm/presentation/bloc/get_all_burung_tersedia/get_all_burung_tersedia_event.dart';
+import 'package:canaryfarm/presentation/bloc/get_all_burung_tersedia/get_all_burung_tersedia_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class GetBurungTersediaBloc
+    extends Bloc<GetBurungTersediaEvent, GetBurungTersediaState> {
+  final GetAllBurungTersediaRepository getAllBurungTersediaRepository;
+
+  GetBurungTersediaBloc(this.getAllBurungTersediaRepository)
+      : super(GetBurungTersediaInitial()) {
+    on<GetAllBurungTersediaEvent>(_getAllBurungTersedia);
+  }
+
+  Future<void> _getAllBurungTersedia(
+    GetAllBurungTersediaEvent event,
+    Emitter<GetBurungTersediaState> emit,
+  ) async {
+    emit(GetBurungTersediaLoading());
+    final result = await getAllBurungTersediaRepository.getAllBurungTersedia();
+    result.fold(
+      (error) => emit(GetBurungTersediaError(message: error)),
+      (burungTersedia) =>
+          emit(GetBurungTersediaLoaded(burungTersedia: burungTersedia)),
+    );
+  }
+}
