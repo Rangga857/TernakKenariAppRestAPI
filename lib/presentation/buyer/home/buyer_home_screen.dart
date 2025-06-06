04aaa8317151f1e764cb1c08b1f4fc10d21ba604
Rangga857 <azharrangga28@gmail.com>
2025-06-06 12:07:30 +0700

    Create Appbar Home buyer

---
+++ lib/presentation/buyer/home/buyer_home_screen.dart
@@ -0,0 +1,69 @@
+import 'package:canaryfarm/presentation/auth/login_screen.dart';
+import 'package:canaryfarm/presentation/bloc/get_all_burung_tersedia/get_burung_tersedia_bloc.dart';
+import 'package:canaryfarm/presentation/bloc/get_all_burung_tersedia/get_burung_tersedia_event.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class BuyerHomeScreen extends StatefulWidget {
+  const BuyerHomeScreen({super.key});
+
+  @override
+  State<BuyerHomeScreen> createState() => _BuyerHomeScreenState();
+}
+
+class _BuyerHomeScreenState extends State<BuyerHomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<GetBurungTersediaBloc>().add(GetAllBurungTersediaEvent());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Home Screen"),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout, color: Colors.black),
+            onPressed: (){
+              showDialog(
+                context: context, 
+                builder: (context){
+                  return CupertinoAlertDialog(
+                    title: const Text("Konfirmasi"),
+                    content: const Text("Apakah Anda yakin ingin keluar?"),
+                    actions: [
+                      CupertinoDialogAction(
+                        child: const Text("Batal"),
+                        onPressed: (){
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      CupertinoDialogAction(
+                        child: const Text("Keluar"),
+                        onPressed: (){
+                          Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(
+                              builder: (context) => const LoginScreen()
+                            ),
+                            (route) => false
+                          );
+                        }
+                      )
+                    ],
+                  );
+                }
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
