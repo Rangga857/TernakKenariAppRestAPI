61f4963edc0731f6ac005e9af671b1c73595eded
Rangga857 <azharrangga28@gmail.com>
2025-06-07 00:35:19 +0700

    update code model burung semua tersedia

--- lib/data/model/response/burung_semua_tersedia_model.dart
+++ lib/data/model/response/burung_semua_tersedia_model.dart
@@ -1,61 +1,143 @@
 import 'dart:convert';
 
 class BurungSemuaTersediaModel {
-    final String? message;
-    final int? statusCode;
-    final List<Datum>? data;
+  final String message;
+  final int statusCode;
+  final List<DataBurungTersedia> data;
 
-    BurungSemuaTersediaModel({
-        this.message,
-        this.statusCode,
-        this.data,
-    });
+  BurungSemuaTersediaModel({
+    required this.message,
+    required this.statusCode,
+    required this.data,
+  });
 
-    factory BurungSemuaTersediaModel.fromJson(String str) => BurungSemuaTersediaModel.fromMap(json.decode(str));
+  BurungSemuaTersediaModel copyWith({
+    String? message,
+    int? statusCode,
+    List<DataBurungTersedia>? data,
+  }) => BurungSemuaTersediaModel(
+    message: message ?? this.message,
+    statusCode: statusCode ?? this.statusCode,
+    data: data ?? this.data,
+  );
 
-    String toJson() => json.encode(toMap());
+  factory BurungSemuaTersediaModel.fromRawJson(String str) =>
+      BurungSemuaTersediaModel.fromJson(json.decode(str));
 
-    factory BurungSemuaTersediaModel.fromMap(Map<String, dynamic> json) => BurungSemuaTersediaModel(
-        message: json["message"],
-        statusCode: json["status_code"],
-        data: json["data"] == null ? [] : List<Datum>.from(json["data"]!.map((x) => Datum.fromMap(x))),
-    );
+  String toRawJson() => json.encode(toJson());
 
-    Map<String, dynamic> toMap() => {
-        "message": message,
-        "status_code": statusCode,
-        "data": data == null ? [] : List<dynamic>.from(data!.map((x) => x.toMap())),
-    };
+  factory BurungSemuaTersediaModel.fromJson(
+    Map<String, dynamic> json,
+  ) => BurungSemuaTersediaModel(
+    message: json["message"],
+    statusCode: json["status_code"],
+    // data: List<DataBurungTersedia>.from(json["data"].map((x) => DataBurungTersedia.fromJson(x))),
+    data: json["data"] == null
+        ? []
+        : List<DataBurungTersedia>.from(
+            json["data"].map((x) => DataBurungTersedia.fromJson(x)),
+          ),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "message": message,
+    "status_code": statusCode,
+    "data": List<dynamic>.from(data.map((x) => x.toJson())),
+  };
 }
 
-class Datum {
-    final int? id;
-    final String? image;
-    final String? noRing;
-    final String? jenisKelamin;
-    final String? usia;
-    final String? jenisKenari;
-    final int? harga;
-    final String? deskripsi;
-    final String? status;
+class BurungSemuaTersediabyIdModel {
+  final String message;
+  final int statusCode;
+  final DataBurungTersedia data;
 
-    Datum({
-        this.id,
-        this.image,
-        this.noRing,
-        this.jenisKelamin,
-        this.usia,
-        this.jenisKenari,
-        this.harga,
-        this.deskripsi,
-        this.status,
-    });
+  BurungSemuaTersediabyIdModel({
+    required this.message,
+    required this.statusCode,
+    required this.data,
+  });
 
-    factory Datum.fromJson(String str) => Datum.fromMap(json.decode(str));
+  BurungSemuaTersediabyIdModel copyWith({
+    String? message,
+    int? statusCode,
+    DataBurungTersedia? data,
+  }) => BurungSemuaTersediabyIdModel(
+    message: message ?? this.message,
+    statusCode: statusCode ?? this.statusCode,
+    data: data ?? this.data,
+  );
 
-    String toJson() => json.encode(toMap());
+  factory BurungSemuaTersediabyIdModel.fromRawJson(String str) =>
+      BurungSemuaTersediabyIdModel.fromJson(json.decode(str));
 
-    factory Datum.fromMap(Map<String, dynamic> json) => Datum(
+  String toRawJson() => json.encode(toJson());
+
+  factory BurungSemuaTersediabyIdModel.fromJson(Map<String, dynamic> json) =>
+      BurungSemuaTersediabyIdModel(
+        message: json["message"],
+        statusCode: json["status_code"],
+        data: DataBurungTersedia.fromJson(json["data"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+    "message": message,
+    "status_code": statusCode,
+    "data": data.toJson(),
+  };
+}
+
+class DataBurungTersedia {
+  final int id;
+  final String image;
+  final String noRing;
+  final String jenisKelamin;
+  final String usia;
+  final String jenisKenari;
+  final int harga;
+  final String deskripsi;
+  final String status;
+
+  DataBurungTersedia({
+    required this.id,
+    required this.image,
+    required this.noRing,
+    required this.jenisKelamin,
+    required this.usia,
+    required this.jenisKenari,
+    required this.harga,
+    required this.deskripsi,
+    required this.status,
+  });
+
+  DataBurungTersedia copyWith({
+    int? id,
+    String? image,
+    String? noRing,
+    String? jenisKelamin,
+    String? usia,
+    String? jenisKenari,
+    int? harga,
+    String? deskripsi,
+    String? status,
+  }) => DataBurungTersedia(
+    id: id ?? this.id,
+    image: image ?? this.image,
+    noRing: noRing ?? this.noRing,
+    jenisKelamin: jenisKelamin ?? this.jenisKelamin,
+    usia: usia ?? this.usia,
+    jenisKenari: jenisKenari ?? this.jenisKenari,
+    harga: harga ?? this.harga,
+    deskripsi: deskripsi ?? this.deskripsi,
+    status: status ?? this.status,
+  );
+
+  factory DataBurungTersedia.fromRawJson(String str) =>
+      DataBurungTersedia.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory DataBurungTersedia.fromJson(Map<String, dynamic> json) =>
+      DataBurungTersedia(
         id: json["id"],
         image: json["image"],
         noRing: json["no_ring"],
@@ -65,17 +147,17 @@ class Datum {
         harga: json["harga"],
         deskripsi: json["deskripsi"],
         status: json["status"],
-    );
+      );
 
-    Map<String, dynamic> toMap() => {
-        "id": id,
-        "image": image,
-        "no_ring": noRing,
-        "jenis_kelamin": jenisKelamin,
-        "usia": usia,
-        "jenis_kenari": jenisKenari,
-        "harga": harga,
-        "deskripsi": deskripsi,
-        "status": status,
-    };
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "image": image,
+    "no_ring": noRing,
+    "jenis_kelamin": jenisKelamin,
+    "usia": usia,
+    "jenis_kenari": jenisKenari,
+    "harga": harga,
+    "deskripsi": deskripsi,
+    "status": status,
+  };
 }
